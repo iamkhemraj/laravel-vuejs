a3a54686cc5a3f00a649f768543e0ab9a99170ee
Khemraj <khemraj@rwit.io>
2024-09-09 16:44:46 +0530

    Removed unwanted file

--- resources/js/components/Customers.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.get('http://127.0.0.1:8000/api/all-user')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-});
