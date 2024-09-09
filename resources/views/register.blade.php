23e6d9f0714f07185b1dec93890051dbdab20904
Khemraj <khemraj@rwit.io>
2024-09-09 18:43:20 +0530

    create blade file for register customer

---
+++ resources/views/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Laravel & Vue.js</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+   <div id="RegiForm"></div>
+    @vite('resources/js/app.js',)
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</body>
+</html>
