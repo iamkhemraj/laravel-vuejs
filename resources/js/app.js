6801a46dc173b8c9fbffde04dfaa2170e2ae8236
Khemraj <khemraj@rwit.io>
2024-09-09 18:41:26 +0530

    Include  register customer form page

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,15 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import RegiForm from './RegiForm.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// Register the RegiForm component globally if needed
+app.component('RegiForm', RegiForm);
+
+app.mount('#app');
+
+// Alternatively, if you want to mount RegiForm to a different element:
+const regiFormApp = createApp(RegiForm);
+regiFormApp.mount('#RegiForm');
